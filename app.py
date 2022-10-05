eee567b44bfb84030f930e17c83b6be7601464ae
Chinonso Alumona <alumonacg@gmail.com>
2022-10-05 13:58:31 +0100

    Commit for web app

---
+++ app.py
@@ -0,0 +1,29 @@
+import json
+import pickle
+from flask import Flask, request, app, jsonify, url_for, render_template
+import numpy as np
+import pandas as pd
+from app import app
+
+app = Flask(__name__)
+
+#Loading the pickle model
+forest_model = pickle.load(open("forestmodel.pkl","rb"))
+scaler_model = pickle.load(open("scaler.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/prediction_api", methods = ["POST"])
+def prediction_api():
+    data = request.json["data"]
+    print(data)
+    print(np.array(list(data.values())).reshape(1, -1))
+    newdata = scaler_model.transform(np.array(list(data.values())).reshape(1, -1))
+    output = forest_model.predict(newdata)
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__=="__main__":
+    app.run(host = "localhost", port=3000, debug=True)
